3885d10f6e1851f930a8d925ae3519449bb21147
joaperi <jrjeova@hotmail.com>
2022-05-01 21:55:33 -0300

    main apiKey

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const [text, setText] = useState("");
   const [dataUser , setDataUser] = useState({});
 
-  const API_KEY = "RGAPI-846fc82e-c186-4665-877b-75eb1b98702b";
+  const API_KEY = "RGAPI-af5d5193-fd07-409a-b0f7-44b6aefdaa7f";
   const api =
     "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
   const apiMatchID = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/";
